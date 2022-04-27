0787b83b6779a88e6e99e81ee0db959690202731
Nicolas Reyland <nicolas@reyland.net>
2022-04-27 13:53:32 +0200

    added python dependencies

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ if [ ! -f "$PWD/docker-utils.py" ]; then
     echo "Please cd into the git repo directory"
     exit 1
 else
+    python3.10 -m pip install -r requirements.txt
     ln -s "$PWD/docker-utils.py" "$HOME/.local/bin/docker-utils"
 fi
